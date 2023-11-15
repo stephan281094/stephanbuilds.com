79e18bfd1643fb7c314838ff9ece92d3a02fbec3
Stephan de Vries <stephan281094@gmail.com>
2023-11-15 11:48:18 +0100

    Fix HTML entities in meta description

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@ export const SITE_URL = process.env.VERCEL_URL
   : "https://stephanbuilds.com";
 export const SITE_TITLE = "Stephan Builds";
 export const SITE_DESCRIPTION =
-  "Hey, I'm Stephan, a freelance web developer based in Amsterdam, passionate about building high&nbsp;quality&nbsp;websites.";
+  "Hey, I'm Stephan, a freelance web developer based in Amsterdam, passionate about building high-quality websites.";
