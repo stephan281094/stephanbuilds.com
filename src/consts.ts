e3ee7577c2f9d71bd6817a740a6bf28c9e3db5d4
Stephan de Vries <stephan281094@gmail.com>
2024-01-30 11:39:18 +0000

    Update description

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
+export const LOCATION = "Porto, Portugal";
+
 export const SITE_URL = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "https://stephanbuilds.com";
 export const SITE_TITLE = "Stephan Builds";
-export const SITE_DESCRIPTION =
-  "Hey, I'm Stephan, a freelance web developer based in Amsterdam, passionate about building high-quality websites.";
+export const SITE_DESCRIPTION = `Hey, I'm Stephan, a freelance web developer, passionate about building high-quality websites. Working remotely in ${LOCATION}`;
