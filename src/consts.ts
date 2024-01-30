59c2982cad8acac2ac0f42c51df19a38114b620c
Stephan de Vries <stephan281094@gmail.com>
2024-01-30 21:03:22 +0000

    Improve wording (working in → working from)

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,4 @@ export const SITE_URL = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "https://stephanbuilds.com";
 export const SITE_TITLE = "Stephan Builds";
-export const SITE_DESCRIPTION = `Hey, I'm Stephan, a freelance web developer, passionate about building high-quality websites. Working remotely in ${LOCATION}`;
+export const SITE_DESCRIPTION = `Hey, I'm Stephan, a freelance web developer, passionate about building high-quality websites. Working remotely from ${LOCATION}`;
