a135fcaec4bd5fc1a54027a0061580c819130e93
Stephan de Vries <stephan281094@gmail.com>
2025-02-05 13:35:20 +0100

    Update location to Barcelona, Spain

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const LOCATION = "Amsterdam, The Netherlands";
+export const LOCATION = "Barcelona, Spain";
 
 export const SITE_URL = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
