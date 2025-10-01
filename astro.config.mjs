e77b8769f4464b94f795816af92614d7ef254180
Stephan de Vries <stephan281094@gmail.com>
2025-10-01 09:04:40 +0200

    Migrate to Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
+import cloudflare from "@astrojs/cloudflare";
 import markdoc from "@astrojs/markdoc";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import keystatic from "@keystatic/astro";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 import { SITE_URL } from "./src/consts";
-import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: cloudflare(),
   output: "static",
   image: {
     domains: ["picsum.photos"],
--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,5 @@
 export const LOCATION = "Amsterdam, The Netherlands";
 
-export const SITE_URL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "https://stephanbuilds.com";
+export const SITE_URL = "https://stephanbuilds.com";
 export const SITE_TITLE = "Stephan Builds";
 export const SITE_DESCRIPTION = `Hey, I'm Stephan, a freelance web developer, passionate about building high-quality websites. Working remotely from ${LOCATION}`;
